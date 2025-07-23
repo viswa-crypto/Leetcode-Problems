48916184265f6c98db0c8425b900e20d4c163cf4
viswa-crypto <viswasrujith24@gmail.com>
2025-07-23 16:33:36 +0530

    Time: 5 ms (52.9%), Space: 45.3 MB (6.87%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        int res = 0;
+        int n = s.length();
+
+        for (int i = 0; i<n-1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res -= roman.get(s.charAt(i));
+            } else {
+                res += roman.get(s.charAt(i));
+            }
+        }
+        return res+roman.get(s.charAt(n-1));
+    }
+}
