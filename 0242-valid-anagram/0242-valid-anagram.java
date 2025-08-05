d9d690e3d9759dec0643d802fde17c03ab4a892d
viswa-crypto <viswasrujith24@gmail.com>
2025-08-05 11:31:18 +0530

    Time: 3 ms (91.92%), Space: 44.6 MB (43.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        else{
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1,arr2);
+        }
+    }
+}
