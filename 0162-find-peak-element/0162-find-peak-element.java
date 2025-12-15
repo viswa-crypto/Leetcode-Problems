cc3b11a61cfabe6ecb85a9f392f5c1f136bf6028
Viswa Srujith Sai <viswasrujith24@gmail.com>
2025-12-15 22:36:02 +0530

    Time: 1 ms (2.56%), Space: 44.2 MB (18.09%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,14 +1,12 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int max = Integer.MIN_VALUE;
+        int idx = 0;
         for(int i=0;i<nums.length;i++)
         {
+            if(nums[i]>=max) idx = i;
             max = Math.max(max , nums[i]);
         }
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i] == max ) return i;
-        }
-        return 0;
+        return idx;
     }
 }
